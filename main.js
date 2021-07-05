c023b174d10ec6ed3529f6591156e44ea3422940
sarvesh-dongre <76567149+sarvesh-dongre@users.noreply.github.com>
2021-07-05 16:58:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song = "";
+song_2 = "";
+
+
+function preload() {
+    song = "music.mp3";
+    song_2 = "music2.mp3";
+}
+
+
+function setup() {
+    canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
